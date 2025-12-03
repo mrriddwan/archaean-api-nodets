b194e02a058057210375295661122997df46d685
Amir Ridwan <amirridwan.dev@gmail.com>
2025-12-03 23:54:58 +0800

    fix: update table naming

---
+++ prisma/migrations/20251203092940_init/migration.sql
@@ -0,0 +1,73 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `orders` table. All the data in the column will be lost.
+  - You are about to drop the column `productId` on the `orders` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `orders` table. All the data in the column will be lost.
+  - You are about to drop the column `userId` on the `orders` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `products` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `products` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `googleId` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `users` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[google_id]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `product_id` to the `orders` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `orders` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `user_id` to the `orders` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `products` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "orders" DROP CONSTRAINT "orders_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "orders" DROP CONSTRAINT "orders_userId_fkey";
+
+-- DropIndex
+DROP INDEX "orders_productId_idx";
+
+-- DropIndex
+DROP INDEX "orders_userId_idx";
+
+-- DropIndex
+DROP INDEX "users_googleId_key";
+
+-- AlterTable
+ALTER TABLE "orders" DROP COLUMN "createdAt",
+DROP COLUMN "productId",
+DROP COLUMN "updatedAt",
+DROP COLUMN "userId",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "product_id" TEXT NOT NULL,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "user_id" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "products" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "createdAt",
+DROP COLUMN "googleId",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "google_id" TEXT,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- CreateIndex
+CREATE INDEX "orders_user_id_idx" ON "orders"("user_id");
+
+-- CreateIndex
+CREATE INDEX "orders_product_id_idx" ON "orders"("product_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_google_id_key" ON "users"("google_id");
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
