14a68395906876dd3c060e7ec076bd80a7fe91b1
Amir Ridwan <amirridwan.dev@gmail.com>
2025-12-04 15:55:09 +0800

    feat: install pino-pretty + add missing dotenv + add auth routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { userRoutes } from '../features/user';
+import { authRoutes } from '@/features/auth/auth.routes';
 
 const router = Router();
 
 router.use('/users', userRoutes);
-// router.use('/auth', authRoutes);
-// router.use('/posts', postRoutes);
+router.use('/auth', authRoutes);
 
 export const routes = router;
